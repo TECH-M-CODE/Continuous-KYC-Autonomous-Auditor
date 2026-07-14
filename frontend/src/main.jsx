f221241af2a8483ffd31c6acc9ed27b75bd66ce3
24f1000002-art <24f1000002@ds.study.iitm.ac.in>
2026-07-15 02:54:54 +0530

    [Feat]: Sprint 3 Frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import './index.css'
 import App from './App.jsx'
+import { ToastProvider } from './components/ToastContext'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -16,7 +17,10 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ToastProvider>
+        <App />
+      </ToastProvider>
     </QueryClientProvider>
   </StrictMode>,
 )
+
